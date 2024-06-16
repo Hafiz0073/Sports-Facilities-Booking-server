d2aefec90ebd508077e2d58f699a4e016001f9a4
Hafizur Rahman <hrrahman07@gmail.com>
2024-06-16 22:00:44 +0600

    send response middleware created

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,11 +1,19 @@
 import { NextFunction, Request, Response } from "express";
 import { UserServices } from "./user.service";
+import sendResponse from "../../utils/sendResponse";
+import httpStatus from "http-status";
 
 //create admin
 const createAdmin = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const result = await UserServices.createAminIntoDB(req.body);
-    res.status(200).json({
+    // res.status(200).json({
+    //   success: true,
+    //   message: "User created successfully",
+    //   data: result,
+    // });
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
       success: true,
       message: "User created successfully",
       data: result,

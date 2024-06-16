d31da50e3e94862e7661ce0a8c3f36494d976726
Hafizur Rahman <hrrahman07@gmail.com>
2024-06-16 21:35:45 +0600

    not found middlewares added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { UserRoutes } from "./modules/user/user.routes";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import notFound from "./middlewares/notFound";
 const app = express();
 
 //persers
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use("/api", UserRoutes);
 //globalerrorhandler
 app.use(globalErrorHandler);
+//not found
+app.use(notFound);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello next!");

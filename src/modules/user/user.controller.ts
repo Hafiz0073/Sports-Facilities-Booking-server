dd17be3e80de5f4d8d7cffe27ca2fc0010eec0a2
Hafizur Rahman <hrrahman07@gmail.com>
2024-06-18 17:57:52 +0600

    catchAsync middleware added

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,26 +2,23 @@ import { Request, RequestHandler, Response } from "express";
 import { UserServices } from "./user.service";
 import sendResponse from "../../utils/sendResponse";
 import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
 
 //create admin
-const createAdmin: RequestHandler = async (req, res, next) => {
-  try {
-    const result = await UserServices.createAminIntoDB(req.body);
-    // res.status(200).json({
-    //   success: true,
-    //   message: "User created successfully",
-    //   data: result,
-    // });
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "User created successfully",
-      data: result,
-    });
-  } catch (error: unknown) {
-    next(error);
-  }
-};
+const createAdmin = catchAsync(async (req, res, next) => {
+  const result = await UserServices.createAminIntoDB(req.body);
+  // res.status(200).json({
+  //   success: true,
+  //   message: "User created successfully",
+  //   data: result,
+  // });
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "User created successfully",
+    data: result,
+  });
+});
 //update User
 const updateUser = async (req: Request, res: Response) => {
   try {
